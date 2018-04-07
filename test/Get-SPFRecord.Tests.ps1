e0f3fc2ba9a14a706ba6af8374d13df69304e3ee
omniomi <omniomi@gmail.com>
2018-04-06 19:34:06 -0600

    Tests

--- test/Get-SPFRecord.Tests.ps1
+++ test/Get-SPFRecord.Tests.ps1
@@ -3,38 +3,54 @@ $SuppressImportModule = $false
 . $PSScriptRoot\Shared.ps1
 
 InModuleScope PSMailTools {
-
-    Context "Multiple SPF Records" {
-        Mock Resolve-DnsName {
-            [Microsoft.DnsClient.Commands.DnsRecord_TXT]@{
-                name = 'example.com'
-                strings = 'v=spf1','a','mx','-all'
+    Describe "Get-SPFRecord" {
+        Context "Normal Operation" {
+            Mock ReturnSPF -ModuleName PSMailTools {
+                return 'v=spf1 a mx include:_spf.example.com ~all'
             }
-            [Microsoft.DnsClient.Commands.DnsRecord_TXT]@{
-                name = 'example.com'
-                strings = 'v=spf1','a','mx','-all'
+
+            It "Does not return any errors." {
+                { Get-SPFRecord example.com } | Should Not Throw
             }
-        }
 
-        It "Returns multiple records on the same domain correctly." {
-            (Get-SPFRecord example.com).Count | Should be 2
-        }
-
-        It "Displays a warning when there is more than one record on a domain." {
-            ((Get-SPFRecord example.com) 3>&1) -match "more than one SPF record" | Should Be $true
-        }
-    }
-
-    Context "Error Checking" {
-        Mock Resolve-DnsName {
-            [Microsoft.DnsClient.Commands.DnsRecord_TXT]@{
-                name = 'example.com'
-                strings = $null
+            It "Returns a MailTools.Security.SPF.SPFRecord" {
+                Get-SPFRecord example.com | Should BeOfType [MailTools.Security.SPF.SPFRecord]
             }
         }
 
-        It "Error when no record found." {
-            { Get-SPFRecord example.com } | Should Throw
+        Context "Multiple SPF Records" {
+            Mock Resolve-DnsName {
+                [Microsoft.DnsClient.Commands.DnsRecord_TXT]@{
+                    name = 'example.com'
+                    strings = 'v=spf1','a','mx','-all'
+                }
+                [Microsoft.DnsClient.Commands.DnsRecord_TXT]@{
+                    name = 'example.com'
+                    strings = 'v=spf1','a','mx','-all'
+                }
+            }
+
+            It "Displays a warning when there is more than one record on a domain." {
+                ((Get-SPFRecord example.com) 3>&1) -match "more than one SPF record" | Should Be $true
+            }
+
+            It "Returns multiple records on the same domain correctly." {
+                (Get-SPFRecord example.com -WarningAction SilentlyContinue).Count | Should be 2
+            }
+
+        }
+
+        Context "Error Checking" {
+            Mock Resolve-DnsName {
+                [Microsoft.DnsClient.Commands.DnsRecord_TXT]@{
+                    name = 'example.com'
+                    strings = $null
+                }
+            }
+
+            It "Error when no record found." {
+                { Get-SPFRecord example.com } | Should Throw
+            }
         }
     }
 }
