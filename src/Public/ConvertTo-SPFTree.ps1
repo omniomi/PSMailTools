7a1e53fa74fa78c9dd35b33290da905e06c35326
omniomi <omniomi@gmail.com>
2018-04-13 10:51:01 -0600

    Refactored for new Resolve-SpfRecord output.

--- src/Public/ConvertTo-SPFTree.ps1
+++ src/Public/ConvertTo-SPFTree.ps1
@@ -12,62 +12,47 @@ function ConvertTo-SpfTree {
     begin {
         $Script:Objects = New-Object 'System.Collections.Generic.List[System.Object]'
 
-        function CalculateIndent ($Obj) {
-            $Script:IndentTable = @{}
-            foreach ($Row in $Obj) {
-                if ($Row.Parent -eq 1) {
-                    $IndentTable.Add($Row.Id,1)
-                } else {
-                    $n = ($IndentTable[$Row.Parent] + 1)
-                    $IndentTable.Add($Row.Id,$n)
-                }
+        function Indent ([int]$i) {
+            $Out = foreach ($n in 0..$i) {
+                ' |'
             }
+
+            -join $Out
         }
 
-        function IndentRow ($Id) {
-            $Indent = $IndentTable[$Id]
+        function PrintRow ([System.Object]$InputObj) {
+            $Name  = $InputObj.Name
+            $Value = $InputObj.Value
+            $Level = $InputObj.Level
 
-            $Output = for ($i = 0 ; $i -lt $Indent ; $i++) {
-                '| '
-            }
+            (Indent $Level) + ' ' + $Name
 
-            -join $Output
-        }
+            $Mechanisms = $Value.Split(' ')
 
-        function DisplayObject ($Obj) {
-            $Rows = $Obj.Value.Split(' ').ToLower()
-
-            foreach ($Row in $Rows) {
-                switch -Regex ($Row) {
-                    "^a.*" {
-                        (IndentRow $Obj.Id) + $Row
-                        $AObjects = ($Objects | Where { $_.Name -match [regex]"^a.*" -and $_.Parent -eq $Obj.Id })
-
-                        foreach ($AObj in $AObjects) {
-                            DisplayObject $AObj
-                        }
+            foreach ($Mech in $Mechanisms) {
+                switch -Regex ($Mech) {
+                    "^ip(?:4|6):.*" {
+                        (Indent ($Level + 1)) + ' ' + $Mech
                     }
-                    "^mx" {
-                        (IndentRow $Obj.Id) + $Row
-                        $MXObjs = ($Objects | Where { $_.Name -like "mx*" -and $_.Parent -eq $Obj.Id })
 
-                        foreach ($MXObj in $MXObjs ) {
-                            $MXRow = DisplayObject $MXObj
-                            $MXRow
-
-                            if ($MXRow -notmatch [regex]"(?:\d{1,3}\.){3}\d{1,3}") {
-                                $Nested = ($Objects | Where { $_.Parent -eq $MXObj.Id })
-                                DisplayObject $Nested
-                            }
-                        }
+                    "^(?:include|redirect).*" {
+                        $Nested = $Objects | Where-Object { $_.Name -eq $Mech -and $_.Level -eq ($Level + 1) }
+                        PrintRow $Nested
                     }
-                    "ip(?:4|6):.*" { (IndentRow $Obj.Id) + $Row }
-                    "include:.*" { (IndentRow $Obj.Id) + $Row ; DisplayObject ($Objects | Where { $_.Name -eq $Row.Split(':')[-1] }) }
-                    "redirect=.*" { (IndentRow $Obj.Id) + $Row ; DisplayObject ($Objects | Where { $_.Name -eq $Row.Split('=')[-1] }) }
-                    "^v\=.*" { continue }
-                    default { (IndentRow $Obj.Id) + $Row }
+
+                    "^(?:a|mx)" {
+                        $Nested = $Objects | Where-Object { $_.Name -eq $Mech -and $_.Level -eq ($Level + 1) }
+                        PrintRow $Nested
+                    }
+
+                    "^.all$" {
+                        (Indent ($Level + 1)) + ' ' + $Mech
+                    }
+                    "^v=spf1$" { continue }
+                    default { (Indent ($Level + 1)) + ' ' + $Mech }
                 }
             }
+
         }
     }
 
@@ -77,13 +62,11 @@ function ConvertTo-SpfTree {
 
     end {
         try {
-            $RootObject = $Objects | Where { $_.Id -eq 1 }
             '----------------------------------------'
-            'Domain:     ' + $RootObject.Name
-            'SPF Record: ' + $RootObject.Value
+            'Domain:     ' + $Objects[0].Name
+            'SPF Record: ' + $Objects[0].Value
             '----------------------------------------'
-            CalculateIndent ($Objects | Where { $_.Id -ne 1 })
-            DisplayObject $RootObject
+            PrintRow $Objects[0]
             '----------------------------------------'
             $Lookups = @('include','a','mx','ptr','exists')
             'DNS Lookup Count: ' + (((-join $Objects.Value).Split(' ').Split(':') | Where { $_ -in $Lookups }).count) + ' out of 10'
